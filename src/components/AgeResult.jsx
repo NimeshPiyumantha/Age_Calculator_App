baf7b5a5ad91e6580fb4835b8bd3f7076dd07090
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-28 08:57:10 +0530

    Update Component

--- src/components/AgeResult.jsx
+++ src/components/AgeResult.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const AgeResult = ({ age }) => {
   return (
-    <h4 className="mt-10 lg:text-3xl font-bold text-gray-700 dark:text-white sm:text-lg font-mono">
+    <h4 className="mt-10 lg:text-3xl font-semibold text-gray-700 dark:text-white sm:text-lg font-Pooppins">
       You are {age.years} Years {age.months} Months and {age.days} Days old.
     </h4>
   );
