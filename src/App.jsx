83d7c850e43dbed2a80a4b561e90b3fe151d72b1
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-28 09:47:10 +0530

    Update Component

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   };
 
   return (
-    <section className="bg-white dark:bg-gray-900 p-8">
+    <section className="bg-brightWhite p-32 dark:bg-gray-900">
       <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
           Age Calculator
