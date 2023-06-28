cb2c26cabc3d6dc7b3c50ad54fb3df1f8d677175
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-28 08:04:11 +0530

    npm install date-fns --save

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
 import { useState } from "react";
 import AgeCalculatorForm from "./components/AgeCalculatorForm";
 import AgeResult from "./components/AgeResult";
+import {
+  differenceInDays,
+  differenceInMonths,
+  differenceInYears,
+} from "date-fns";
 
 function App() {
-  const [age,setAge]=useState(null);
+  const [age, setAge] = useState(null);
 
-    const calculateAge = (birthDate) => {
-     const today = new Date();
-     const birthDateObj = new Date(birthDate);
-     console.log(today, birthDateObj);
-    }
+  const calculateAge = (birthDate) => {
+    const today = new Date();
+    const birthDateObj = new Date(birthDate);
+    const ageYears = differenceInYears(today, birthDateObj);
+    const ageMonths = differenceInMonths(today, birthDateObj) % 12;
+    const ageDays = differenceInDays(
+      today,
+      new Date(today.getFullYear(), today.getMonth(), birthDateObj.getDate())
+    );
+
+    setAge({
+      years: ageYears,
+      months: ageMonths,
+      days: ageDays,
+    });
+  };
 
   return (
     <>
       <h1>Age Calculator</h1>
       <AgeCalculatorForm calculateAge={calculateAge} />
-      <AgeResult/>
+      <AgeResult />
     </>
   );
 }
