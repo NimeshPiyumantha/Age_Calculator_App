e904a2761007322f5e75d6849ab6270c7ca1c893
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-28 08:21:11 +0530

    Update Component

--- src/App.jsx
+++ src/App.jsx
@@ -28,11 +28,18 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Age Calculator</h1>
-      <AgeCalculatorForm calculateAge={calculateAge} />
-      {age && <AgeResult age={age} />}
-    </div>
+    <section className="bg-white dark:bg-gray-900">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          Age Calculator
+        </h1>
+        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+          Track Your Life&apos;s Progress: Age in Years, Months, Days
+        </p>
+        <AgeCalculatorForm calculateAge={calculateAge} />
+        {age && <AgeResult age={age} />}
+      </div>
+    </section>
   );
 }
 
