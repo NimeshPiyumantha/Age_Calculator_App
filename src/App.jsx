1ea864c0a01556acf230badbc7125ffd0b6aeef8
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-28 07:51:01 +0530

    Update Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import AgeCalculatorForm from "./components/AgeCalculatorForm";
 import AgeResult from "./components/AgeResult";
 
 function App() {
+  const [age,setAge]=useState(null);
 
-    const calculateAge = () => {
-      console.log("cal age");
+    const calculateAge = (birthDate) => {
+     const today = new Date();
+     const birthDateObj = new Date(birthDate);
+     console.log(today, birthDateObj);
     }
 
   return (
